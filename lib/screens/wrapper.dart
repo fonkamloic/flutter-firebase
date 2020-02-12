defd652d5c3ff0692a229caed98a852a6273878a
fonkamloic <fonkamloic@gmail.com>
2020-02-12 05:27:17 +0100

    navigator_wrapper

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/models/user.dart';
+import 'package:flutter_firebase/screens/authenticate/authenticate.dart';
+import 'package:flutter_firebase/screens/home/home.dart';
+import 'package:provider/provider.dart';
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print(user);
+
+    if(user == null){
+      return Authenticate();
+    }else{
+      return Home();
+
+    }
+
+  }
+}
