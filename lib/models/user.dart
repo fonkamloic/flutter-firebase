a53cbb5cf33ca13c15dc4ec9460f933ddccca2e1
fonkamloic <fonkamloic@gmail.com>
2020-02-12 05:28:24 +0100

    user_component

---
+++ lib/models/user.dart
@@ -0,0 +1,14 @@
+class User {
+  final String uid;
+
+  User({this.uid});
+}
+
+class UserData {
+  final String uid;
+  final String name;
+  final String sugars;
+  final int strength;
+
+  UserData({this.name, this.sugars, this.strength, this.uid});
+}
