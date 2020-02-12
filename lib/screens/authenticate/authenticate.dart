bf961f4e19e7d0d44fc65da4f61231a87a4ca93c
fonkamloic <fonkamloic@gmail.com>
2020-02-12 05:28:43 +0100

    authentication_component

---
+++ lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/screens/authenticate/register.dart';
+import 'package:flutter_firebase/screens/authenticate/sign_in.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+
+
+  bool showSignIn = true;
+
+
+  void toogleView(){
+    setState(() {
+      showSignIn = !showSignIn;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    if(showSignIn){
+      return SignIn(toogleView: toogleView);
+    }else{
+      return Register(toogleView: toogleView);
+    }
+  }
+}
